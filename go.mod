ad884c8ce03c147c81818c9c9f9054f7a80a45f8
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-02 11:47:32 +0900

    day 1 may

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bananaumai/leetcode-challenge
+
+go 1.13
---
+++ may_1st/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+var (
+	n = 3
+	badVersion = 2
+	apiCallCnt int
+)
+
+func main() {
+	answer := firstBadVersion(n)
+	fmt.Printf("answer: %d, api call cnt: %d\n", answer, apiCallCnt)
+}
+
+func firstBadVersion(n int) int {
+	return solve(0, n, 0)
+}
+
+func solve(start int, end int, answer int) int {
+	if start == end {
+		if isBadVersion(start) {
+			return start
+		}
+		return answer
+	}
+
+	if start + 1 == end {
+		if isBadVersion(start) {
+			return start
+		}
+		if isBadVersion(end) {
+			return end
+		}
+		return answer
+	}
+
+	half := (start + end) / 2
+
+	if isBadVersion(half) {
+		return solve(start, half-1, half)
+	}
+
+	return solve(half+1, end, answer)
+}
+
+func isBadVersion(version int) bool {
+	apiCallCnt++
+	return version >= badVersion
+}
