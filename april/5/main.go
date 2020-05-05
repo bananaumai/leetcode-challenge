566347d8672316f7c2742e26b4934fd28490c75b
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-06 01:08:32 +0900

    improve : Day 5 April -- simple answer from
    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3287/discuss/39420/Three-lines-in-C++-with-explanation

--- april/5/main.go
+++ april/5/main.go
@@ -1,50 +1,11 @@
 package main
 
 func maxProfit(prices []int) int {
-	return newSolver(prices).solve(0)
-}
-
-const dpInit = -1
-
-type solver struct {
-	prices []int
-	dp []int
-}
-
-func newSolver(prices []int) solver {
-	dpSize := len(prices)
-	dp := make([]int, dpSize)
-	for i := 0; i < dpSize; i++ {
-		dp[i] = dpInit
-	}
-	return solver{prices, dp}
-}
-
-func (s solver) solve(start int) int {
-	answer := s.dp[start]
-	if answer != dpInit {
-		return answer
-	}
-
-	max := 0
-	for i := start; i < len(s.prices); i++ {
-		for j := i + 1; j < len(s.prices); j++ {
-			profit := s.prices[j] - s.prices[i]
-
-			if j + 1 < len(s.prices) {
-				rest := s.solve(j+1)
-				if profit >= 0 {
-					profit += rest
-				} else {
-					profit = rest
-				}
-			}
-
-			if profit > max {
-				max = profit
-			}
+	var total int
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			total += prices[i] - prices[i-1]
 		}
 	}
-	s.dp[start] = max
-	return max
+	return total
 }
