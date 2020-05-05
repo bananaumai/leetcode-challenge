cb1462c901f360454e8db0a92ea77295252e9d0f
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-05 11:20:42 +0900

    improve

--- april/3/main.go
+++ april/3/main.go
@@ -3,26 +3,23 @@ package main
 import "math"
 
 func maxSubArray(nums []int) int {
-	m := math.MinInt64
+	dp := make([][]int, len(nums))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(nums))
+	}
+
+	max := math.MinInt64
 	for i := 0; i < len(nums); i++ {
-		for k := i; k < len(nums); k++ {
-			m = max(m, sum(nums[i:k+1]))
+		for j := i; j < len(nums); j++ {
+			if j == i {
+				dp[i][j] = nums[j]
+			} else {
+				dp[i][j] = dp[i][j-1] + nums[j]
+			}
+			if dp[i][j] > max {
+				max = dp[i][j]
+			}
 		}
 	}
-	return m
-}
-
-func max(a, b int) int {
-	if a >= b {
-		return a
-	}
-	return b
-}
-
-func sum(nums []int) int {
-	var acc int
-	for _, n := range nums {
-		acc += n
-	}
-	return acc
+	return max
 }
