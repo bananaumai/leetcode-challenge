adaa604edaf7babaa37934394c39293d5afd0434
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-06 11:14:08 +0900

    Day 8 April

---
+++ 2020-04-08/main.go
@@ -0,0 +1,44 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	var nodes []*ListNode
+
+	next := head
+	for {
+		if next == nil {
+			break
+		}
+
+		nodes = append(nodes, next)
+
+		next = next.Next
+	}
+
+	if nodes == nil {
+		return nil
+	}
+
+	size := len(nodes)
+
+	if size == 0 {
+		return nil
+	}
+
+	if size == 1 {
+		return nodes[0]
+	}
+
+	return nodes[size/2]
+}
---
+++ 2020-04-08/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func (n *ListNode) String() string {
+	next := n
+
+	var vs []string
+
+	for {
+		if next == nil {
+			break
+		}
+
+		vs = append(vs, fmt.Sprintf("%d", next.Val))
+
+		next = next.Next
+	}
+
+	return fmt.Sprintf("[%s]", strings.Join(vs, ", "))
+}
+
+func Test_middleNode(t *testing.T) {
+	tts := []struct {
+		input []int
+		expected int
+	} {
+		{[]int{1, 2, 3, 4, 5}, 3},
+		{[]int{1, 2, 3, 4, 5, 6}, 4},
+		{[]int{1}, 1},
+		{[]int{1, 2}, 2},
+	}
+
+	for _, tt := range tts {
+		tt := tt
+
+		t.Run(fmt.Sprintf("input %v", tt.input), func(t *testing.T) {
+			t.Parallel()
+
+			var nodes []*ListNode
+			for _, n := range tt.input {
+				nodes = append(nodes, &ListNode{Val: n})
+			}
+			t.Logf("nodes: %v", nodes)
+			for i := len(nodes) - 1; i > 0; i-- {
+				nodes[i-1].Next = nodes[i]
+			}
+			t.Logf("nodes: %v", nodes)
+
+			head := nodes[0]
+
+			actual := middleNode(head).Val
+
+			if tt.expected != actual {
+				t.Errorf("expected: %d <=> actual: %d (%s)", tt.expected, actual, head)
+			}
+		})
+	}
+}
