1fcb43701c11e81e3fb5b79a42980b9db4dbdcf9
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-07 05:50:53 +0900

    Day 11 April - improve 2

--- 2020/04/11/main.go
+++ 2020/04/11/main.go
@@ -6,74 +6,24 @@ type TreeNode struct {
     Right *TreeNode
 }
 
-type path []string
-
 func diameterOfBinaryTree(root *TreeNode) int {
-	paths := traverse(root)
-
-	if paths == nil || len(paths) == 0 {
-		return 0
-	}
-
-	if len(paths) == 1 {
-		return len(paths[0])
-	}
-
-	var longestPath path
-	for _, path := range paths {
-		if len(path) > len(longestPath) {
-			longestPath = path
-		}
-	}
-
-	max := len(longestPath)
-	for _, path := range paths {
-		var commonLen int
-		for i := 0; i < len(path); i++ {
-			if path[i] == longestPath[i] {
-				commonLen++
-			} else {
-				break
-			}
-		}
-		total := len(path) + len(longestPath) - commonLen * 2
-		if total > max {
-			max = total
-		}
-	}
-
-	return max
+	c, m := diameter(root)
+	return max(c - 1, m)
 }
 
-func traverse(node *TreeNode) []path {
+func diameter(node *TreeNode) (int, int) {
 	if node == nil {
-		return nil
+		return 0, 0
 	}
 
-	var paths []path
+	lCount, lMax := diameter(node.Left)
+	rCount, rMax := diameter(node.Right)
+	return max(lCount, rCount) + 1, max(lCount + rCount, max(lMax, rMax))
+}
 
-	for _, direction := range []string{"l", "r"} {
-		var target *TreeNode
-		if direction == "l" {
-			target = node.Left
-		} else {
-			target = node.Right
-		}
-
-		if target == nil {
-			continue
-		}
-
-		tails := traverse(target)
-		if tails != nil {
-			for _, path := range tails {
-				newPath := append([]string{direction}, path...)
-				paths = append(paths, newPath)
-			}
-		} else {
-			paths = append(paths, []string{direction})
-		}
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-
-	return paths
+	return b
 }
