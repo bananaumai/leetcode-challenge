3cd77a5ef09e96feb388f00452a02926aa33fdbf
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-05-09 11:08:01 +0900

    Day 8 May

--- 2020/05/08/main.go
+++ 2020/05/08/main.go
@@ -1,1 +1,28 @@
-package _8
+package main
+
+func checkStraightLine(coordinates [][]int) bool {
+	if len(coordinates) <= 1 {
+		return true
+	}
+
+	var firstRoc float64
+
+	for i := 1; i < len(coordinates); i++ {
+		roc := rateOfChange(coordinates[i], coordinates[i-1])
+
+		if firstRoc == 0 {
+			firstRoc = roc
+			continue
+		}
+
+		if firstRoc != roc {
+			return false
+		}
+	}
+
+	return true
+}
+
+func rateOfChange(c2, c1 []int) float64 {
+	return (float64(c2[1]) - float64(c1[1])) / (float64(c2[0]) - float64(c1[0]))
+}
--- 2020/05/08/main_test.go
+++ 2020/05/08/main_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_checkStraightLine(t *testing.T) {
+	tts := []struct {
+		input    [][]int
+		expected bool
+	}{
+		{[][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {6, 7}}, true},
+		{[][]int{{1, 1}, {2, 2}, {3, 4}, {4, 5}, {5, 6}, {7, 7}}, false},
+	}
+
+	for _, tt := range tts {
+		tt := tt
+
+		t.Run(fmt.Sprintf("input %v", tt.input), func(t *testing.T) {
+			t.Parallel()
+
+			actual := checkStraightLine(tt.input)
+
+			if tt.expected != actual {
+				t.Errorf("expected: %v <=> actual: %v", tt.expected, actual)
+			}
+		})
+	}
+}
